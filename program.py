1852b4b4eb3a41cdfe47259028f99bc4cf09e7e7
Mussab Asif <mussabintel@gmail.com>
2024-09-27 22:48:09 -0400

    game introduction

--- program.py
+++ program.py
@@ -1,5 +1,16 @@
 #Assignment 1
 #PROG10004
 #Muhammad Mussab Asif 
-print("hello",)
-print("my nam")
+
+#importing the function 'choice' from the 'random' library 
+from random import choice
+
+print(" = || 8< = || 8<= || 8< ROCK PAPER SCISSORS = || 8< = || 8<= || 8< ")
+print("Welcome to the game of ROCK PAPER SCISSORS")
+
+def gameStart():
+    numberOfRounds = 0
+    while numberOfRounds <= 1:
+        numberOfRounds = int(input("Enter the number of rounds you want to play: "))
+    print("May the best one win " + numberOfRounds + "games!")
+    
